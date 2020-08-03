a5f8f8bf6ba976f6f6a239da93279e173809cc98
Mark Kooijman <kooi.barlaeus@gmail.com>
2020-08-03 17:15:15 +0200

    image-viewer update

--- pixie_led.py
+++ pixie_led.py
@@ -165,7 +165,7 @@ def fake_task(n):
 
 
 def panel_gif(filename, loop=10, delay=0.2):
-    os.system("sudo /home/ubuntu/rpi-rgb-led-matrix/utils/led-image-viewer /home/ubuntu/pixie/cache/temp3.gif")
+    os.system("sudo /home/ubuntu/rpi-rgb-led-matrix/utils/led-image-viewer /home/ubuntu/pixie/cache/temp3.gif -t 5")
     # global matrix, offscreen_canvas
     # image = Image.open(filename)
     # for l in range(loop):
