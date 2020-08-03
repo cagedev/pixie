bc3a4b2638e6a5eeddea8aa1c6d62e6abd4792c5
Mark Kooijman <kooi.barlaeus@gmail.com>
2020-08-03 12:47:46 +0200

    rgbmatrix tweaks for background performance 1/n

--- pixie-led.py
+++ pixie-led.py
@@ -32,8 +32,8 @@ options.parallel = 1
 options.hardware_mapping = 'adafruit-hat'
 options.disable_hardware_pulsing = False  # default: False
 options.pwm_bits = 8  # default: 11
-options.pwm_lsb_nanoseconds = 200  # default: 130
-options.gpio_slowdown = 3  # default: 2
+options.pwm_lsb_nanoseconds = 50  # default: 130
+options.gpio_slowdown = 2  # default: 2
 options.drop_privileges = False
 
 matrix = RGBMatrix(options=options)
