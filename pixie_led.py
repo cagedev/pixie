bc65893c722d286f43f2269a7a7029047d651d4a
Mark Kooijman <kooi.barlaeus@gmail.com>
2020-08-07 19:32:12 +0200

    typo

--- pixie_led.py
+++ pixie_led.py
@@ -151,7 +151,7 @@ def upload():
 def show_list():
     files = []
     for directory in app.config['IMAGE_FILE_DIRS']:
-        for file in os.listdir(directory)]):
+        for file in os.listdir(directory):
             filename = os.path.join(directory, file)
             extension = os.path.splitext(fullname)[1].lower()
             files.append({
